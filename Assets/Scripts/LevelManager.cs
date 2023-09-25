18bca7846487888f0992881ed2225d6c556831f1
dwimynt <dwi.miyanto@amikom.ac.id>
2023-09-25 07:28:48 +0700

    setup project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager _instance = null;
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<LevelManager>();
+            }
+            return _instance;
+        }
+    }
+    [SerializeField] private Transform _towerUIParent;
+    [SerializeField] private GameObject _towerUIPrefab;
+    [SerializeField] private Tower[] _towerPrefabs;
+    private void Start()
+    {
+        InstantiateAllTowerUI();
+    }
+
+    // Menampilkan seluruh Tower yang tersedia pada UI Tower Selection
+    private void InstantiateAllTowerUI()
+    {
+        foreach (Tower tower in _towerPrefabs)
+        {
+            GameObject newTowerUIObj = Instantiate
+            (_towerUIPrefab.gameObject, _towerUIParent);
+            TowerUI newTowerUI = newTowerUIObj.GetComponent<TowerUI>();
+            newTowerUI.SetTowerPrefab(tower);
+            newTowerUI.transform.name = tower.name;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TowerUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerUI : MonoBehaviour
+{
+    [SerializeField] private Image _towerIcon;
+    private Tower _towerPrefab;
+    public void SetTowerPrefab(Tower tower)
+    {
+        _towerPrefab = tower;
+        _towerIcon.sprite = tower.GetTowerHeadIcon();
+    }
+
+}
