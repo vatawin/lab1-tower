9807afb3e1f590ef2aa708850d4d74b201df120c
vatawin <136071697+vatawin@users.noreply.github.com>
2023-09-29 14:52:10 +0700

    game update
    game update ke 2

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 1;
+    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] private SpriteRenderer _healthBar;
+    [SerializeField] private SpriteRenderer _healthFill;
+    private int _currentHealth;
+    // Fungsi ini terpanggil sekali setiap kali menghidupkan game object yang
+   
+private void OnEnable()
+    {
+        _currentHealth = _maxHealth;
+        _healthFill.size = _healthBar.size;
+    }
+}
